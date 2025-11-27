acf3e23df26f66242fc4439de3fc3629c2869df2
Valerii Petryniak <valerii15298@gmail.com>
2025-11-27 04:15:38 +0200

    refactor: remove unused useOpenAPI import from OperationPlayground

--- src/openapi/operation-playground/index.tsx
+++ src/openapi/operation-playground/index.tsx
@@ -5,7 +5,6 @@ import { useMemo, useState } from "react";
 import { K } from "#openapi/const";
 import {
   useHttpProxy,
-  useOpenAPI,
   useOperation,
   useOperationState,
 } from "#openapi/context";

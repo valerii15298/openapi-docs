5fc9a526d05ad30dd56dcf2d81956d3882b163d6
Valerii Petryniak <valerii15298@gmail.com>
2026-02-04 16:39:08 +0200

    feat: add react-error-boundary for improved error handling in Docs component

--- src/openapi/docs.tsx
+++ src/openapi/docs.tsx
@@ -1,4 +1,12 @@
-import { SidebarProvider } from "@sane-ts/base-shadcn";
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+  Empty,
+  EmptyHeader,
+  SidebarProvider,
+} from "@sane-ts/base-shadcn";
+import { ErrorBoundary } from "react-error-boundary";
 
 import { OpenAPIContext } from "#openapi/context";
 import { matchRoute } from "#openapi/router";
@@ -8,15 +16,31 @@ import { StorageProvider } from "#storage";
 export function Docs(ctx: OpenAPIContext) {
   const renderRoute = matchRoute(ctx.path);
   return (
-    <StorageProvider buster={ctx.uri}>
-      <OpenAPIContext value={ctx}>
-        <SidebarProvider className="relative h-full min-h-full">
-          <SideBar />
-          <main className="flex-1 overflow-auto">
-            {renderRoute?.(ctx.path)}
-          </main>
-        </SidebarProvider>
-      </OpenAPIContext>
-    </StorageProvider>
+    <ErrorBoundary
+      fallbackRender={({ error }) => {
+        const err = Error.isError(error) ? error : new Error(String(error));
+        return (
+          <Empty>
+            <EmptyHeader>
+              <Alert variant={"destructive"}>
+                <AlertTitle>{err.name}</AlertTitle>
+                <AlertDescription>{err.message}</AlertDescription>
+              </Alert>
+            </EmptyHeader>
+          </Empty>
+        );
+      }}
+    >
+      <StorageProvider buster={ctx.uri}>
+        <OpenAPIContext value={ctx}>
+          <SidebarProvider className="relative h-full min-h-full">
+            <SideBar />
+            <main className="flex-1 overflow-auto">
+              {renderRoute?.(ctx.path)}
+            </main>
+          </SidebarProvider>
+        </OpenAPIContext>
+      </StorageProvider>
+    </ErrorBoundary>
   );
 }

242c487490102de1341971ce8a7489a05e2ee5bd
Valerii Petryniak <valerii15298@gmail.com>
2026-02-18 22:04:48 +0200

    fix: update Intro component styling for improved layout and alignment

--- src/openapi/intro.tsx
+++ src/openapi/intro.tsx
@@ -5,8 +5,8 @@ import { ExternalDocs } from "#openapi/operation-docs/external-docs";
 export function Intro() {
   const { doc: spec } = useOpenAPI();
   return (
-    <section className="m-4 flex flex-col gap-4">
-      <h1 className="flex flex-wrap items-end justify-between text-6xl font-extrabold tracking-tight text-balance">
+    <section className="mx-auto my-4 flex max-w-5xl flex-col gap-4 px-4">
+      <h1 className="flex flex-wrap items-end justify-between text-center text-6xl font-extrabold tracking-tight text-balance">
         {spec.info?.summary}
         <ExternalDocs externalDocs={spec.externalDocs} path={[]} />
       </h1>
